2871c592d04aa0a69019b48e4a7802fc6e3779aa
Emrecan Dönmez <edonmez8@ford.com.tr>
2023-11-11 11:49:31 +0300

    Add project files.

---
+++ PaginationAPI/Models/NyctaxiSample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaginationDemo.Models;
+
+public partial class NyctaxiSample
+{
+    public string Medallion { get; set; } = null!;
+
+    public string HackLicense { get; set; } = null!;
+
+    public string? VendorId { get; set; }
+
+    public string? RateCode { get; set; }
+
+    public string? StoreAndFwdFlag { get; set; }
+
+    public DateTime PickupDatetime { get; set; }
+
+    public DateTime? DropoffDatetime { get; set; }
+
+    public int? PassengerCount { get; set; }
+
+    public long? TripTimeInSecs { get; set; }
+
+    public double? TripDistance { get; set; }
+
+    public string? PickupLongitude { get; set; }
+
+    public string? PickupLatitude { get; set; }
+
+    public string? DropoffLongitude { get; set; }
+
+    public string? DropoffLatitude { get; set; }
+
+    public string? PaymentType { get; set; }
+
+    public double? FareAmount { get; set; }
+
+    public double? Surcharge { get; set; }
+
+    public double? MtaTax { get; set; }
+
+    public double? TollsAmount { get; set; }
+
+    public double? TotalAmount { get; set; }
+
+    public double? TipAmount { get; set; }
+
+    public int? Tipped { get; set; }
+
+    public int? TipClass { get; set; }
+}
---
+++ PaginationAPI/Models/NyctaxiSampleContext.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaginationDemo.Models;
+
+public partial class NyctaxiSampleContext : DbContext
+{
+    public NyctaxiSampleContext()
+    {
+    }
+
+    public NyctaxiSampleContext(DbContextOptions<NyctaxiSampleContext> options)
+        : base(options)
+    {
+    }
+
+
+    public virtual DbSet<NyctaxiSample> NyctaxiSamples { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");     
+
+        modelBuilder.Entity<NyctaxiSample>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("nyctaxi_sample");
+
+            entity.Property(e => e.DropoffDatetime)
+                .HasColumnType("datetime")
+                .HasColumnName("dropoff_datetime");
+            entity.Property(e => e.DropoffLatitude)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("dropoff_latitude");
+            entity.Property(e => e.DropoffLongitude)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("dropoff_longitude");
+            entity.Property(e => e.FareAmount).HasColumnName("fare_amount");
+            entity.Property(e => e.HackLicense)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("hack_license");
+            entity.Property(e => e.Medallion)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("medallion");
+            entity.Property(e => e.MtaTax).HasColumnName("mta_tax");
+            entity.Property(e => e.PassengerCount).HasColumnName("passenger_count");
+            entity.Property(e => e.PaymentType)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("payment_type");
+            entity.Property(e => e.PickupDatetime)
+                .HasColumnType("datetime")
+                .HasColumnName("pickup_datetime");
+            entity.Property(e => e.PickupLatitude)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("pickup_latitude");
+            entity.Property(e => e.PickupLongitude)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("pickup_longitude");
+            entity.Property(e => e.RateCode)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("rate_code");
+            entity.Property(e => e.StoreAndFwdFlag)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("store_and_fwd_flag");
+            entity.Property(e => e.Surcharge).HasColumnName("surcharge");
+            entity.Property(e => e.TipAmount).HasColumnName("tip_amount");
+            entity.Property(e => e.TipClass).HasColumnName("tip_class");
+            entity.Property(e => e.Tipped).HasColumnName("tipped");
+            entity.Property(e => e.TollsAmount).HasColumnName("tolls_amount");
+            entity.Property(e => e.TotalAmount).HasColumnName("total_amount");
+            entity.Property(e => e.TripDistance).HasColumnName("trip_distance");
+            entity.Property(e => e.TripTimeInSecs).HasColumnName("trip_time_in_secs");
+            entity.Property(e => e.VendorId)
+                .HasMaxLength(3)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("vendor_id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
